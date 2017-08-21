5f02731ed3e3c109334e062743c8d4b28d463b2f
Gabriel Kabbe <gabriel.kabbe@mail.de>
2017-08-21 16:42:53 +0200

    Somehow working

--- cartpole/main.py
+++ cartpole/main.py
@@ -4,7 +4,7 @@ import sys
 import pickle
 import logging
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 import gym
 import numpy as np
@@ -14,18 +14,23 @@ import tensorflow as tf
 logging.getLogger("gym").setLevel(logging.WARN)
 
 
-def NeuralNet(input_size: int, hidden_size: int, output_size: int):
+RENDER = True
+
+
+def create_neural_net(input_size: int, hidden_size: int, output_size: int):
     input_ = tf.placeholder(dtype=tf.float32, shape=[None, input_size], name="input")
-    output = tf.placeholder(dtype=tf.float32, shape=[None, output_size], name="output")
+    actual_reward = tf.placeholder(dtype=tf.float32, shape=[None, 1],
+                                   name="actual_reward")
     W1 = tf.Variable(tf.random_normal([input_size, hidden_size]))
     b1 = tf.Variable(tf.random_normal([hidden_size]))
     W2 = tf.Variable(tf.random_normal([hidden_size, output_size]))
     b2 = tf.Variable(tf.random_normal([output_size]))
     hidden = tf.tanh(tf.matmul(input_, W1) + b1, "hidden")
-    predicted_q = tf.matmul(hidden, W2) + b2
-    action = tf.argmax(predicted_q, axis=1)
+    all_q = tf.matmul(hidden, W2) + b2
+    predicted_actions = tf.argmax(all_q, axis=1)
+    predicted_q = tf.reduce_max(all_q, axis=1)
 
-    return input_, output, predicted_q, action
+    return input_, actual_reward, predicted_q, predicted_actions
 
 
 # R_t = r_t + gamma * R_{t+1}
@@ -39,40 +44,51 @@ def determine_cumulative_reward(rewards, *, gamma):
 
 
 def main():
-    input_size, hidden_size, output_size = 4, 10, 2
+    logfile = open("log.out", "w")
+    input_size, hidden_size, output_size = 4, 50, 2
     gamma = 0.9
 
     max_steps = 1000
     epochs = 1000
-    
-    input_, output, predicted_q, predicted_action = NeuralNet(input_size, hidden_size, output_size)
-    rewards_ = tf.placeholder(dtype=tf.float32, shape=4)
-    session = tf.InteractiveSession()
-    session.run(tf.global_variables_initializer())
 
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    #loss = output[:-1] - gamma * output[1:] - rewards_
-    # train = optimizer.minimize(loss)
+    input_, actual_reward, predicted_q, predicted_action = create_neural_net(input_size,
+                                                                             hidden_size,
+                                                                             output_size)
+    session = tf.InteractiveSession()
+
+    optimizer = tf.train.AdadeltaOptimizer()
+    loss = tf.reduce_sum(tf.square(predicted_q - gamma * predicted_q - actual_reward))
+    #delta = predicted_q - gamma * predicted_q - actual_reward
+    #loss = tf.losses.huber_loss(delta, tf.zeros(shape=tf.shape(delta)))
+    train = optimizer.minimize(loss)
+    session.run(tf.global_variables_initializer())
 
     env = gym.make('CartPole-v0')
 
-    
+
     observables_total = []
     rewards_total = []
 
     for epoch in range(epochs):
         observables = []
-        actions = []
+        guessed_actions = []
         rewards = []
+        guessed_cumulative_rewards = []
+
         net_outputs = []
         logger.info("Epoch: {}".format(epoch))
         env.reset()
         obs = env.observation_space.sample()
+        observables.append(obs)
         for step in range(1000):
-            env.render()
-            guessed_action, = session.run(predicted_action, {input_: obs[None, :]})
+            if RENDER:
+                env.render()
+            gcr, guessed_action = session.run([predicted_q, predicted_action],
+                                              {input_: obs[None, :]})
+            guessed_action = guessed_action[0]
+            guessed_cumulative_rewards.append(gcr.squeeze())
             obs, reward, done, info = env.step(guessed_action)
-            actions.append(guessed_action)
+            guessed_actions.append(guessed_action)
             observables.append(obs)
             logger.info("Step".format(step))
             if done:
@@ -85,18 +101,25 @@ def main():
         out_arr = np.array(net_outputs)
         cumulative_rewards = determine_cumulative_reward(rewards, gamma=gamma)
         rewards = np.array(rewards)
-        #guessed_actions = out_arr[range(len(out_arr)), actions]
-        delta = cumulative_rewards[:-1] - gamma * cumulative_rewards[1:] - rewards[:-1]
-        logger.debug("Observables: {}".format(observables))
-        logger.debug("Actions: {}".format(actions))
-        logger.debug("Rewards: {}".format(cumulative_rewards))
+        guessed_cumulative_rewards = np.array(guessed_cumulative_rewards)
+        observables = np.array(observables)
+        #guessed_actions = out_arr[range(len(out_arr)), guessed_actions]
+        #rewards_of_chosen_actions = guessed_cumulative_rewards[range(guessed_cumulative_rewards.shape[0]), guessed_actions]
+        #delta = rewards_of_chosen_actions[:-1] - gamma * rewards_of_chosen_actions[1:] - rewards[:-1]
+        losstmp = session.run(loss, {input_: observables, actual_reward: rewards[:, None]})
+        session.run(train, {input_: observables, actual_reward: rewards[:, None]})
+        logger.info(f"Loss is {losstmp}")
+        print(f"Loss is {losstmp}", file=logfile, flush=True)
+        logger.debug(f"Observables: {observables}")
+        logger.debug(f"Actions: {guessed_actions}")
+        logger.debug(f"Rewards: {cumulative_rewards}")
         #  print("Guessed:", guessed_actions)
-        logger.debug("Diffs: {}".format(delta))
+        #logger.debug("Delta: {}".format(delta))
 
-        rewards_total.append(delta)
-        
-        with open("data", "wb") as f:
-            pickle.dump({"observables": observables_total, "rewards": rewards_total}, f)
+        #rewards_total.append(delta)
+
+        #with open("data", "wb") as f:
+        #    pickle.dump({"observables": observables_total, "rewards": rewards_total}, f)
 
 
 if __name__ == "__main__":
