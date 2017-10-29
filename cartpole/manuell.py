caa6f034ddbbe639371031fda9a66feda504b462
Gabriel Kabbe <gabriel.kabbe@mail.de>
2017-10-29 20:07:00 +0100

    Update manuell

--- cartpole/manuell.py
+++ cartpole/manuell.py
@@ -2,6 +2,7 @@ from itertools import count
 import random
 
 import gym
+import numpy as np
 import tensorflow as tf
 
 def Neuro():
@@ -27,29 +28,50 @@ def update_net(net, factor, opt):
     opt.apply_gradients(grads_and_vars)
 
 
+def TrainingOp(optimizer, grad_var_list):
+    grad_placeholders = [(tf.placeholder(tf.float32, shape=grad.get_shape()), var)
+                         for grad, var in grad_var_list]
+    return optimizer.apply_gradients(grad_placeholders)
+
+
+def calculate_mean_gradient(states, actions, input_, dp0dW):
+    grads = [g.eval(feed_dict={input_: np.vstack(states[:-1])}) for g, v in dp0dW]
+    import ipdb; ipdb.set_trace()
+
+
 def main():
     input_, neuro = Neuro()
     optimizer = Optimizer()
     init = tf.global_variables_initializer()    
-    sess = tf.InteractiveSession()
-    init.run()
 
     env = gym.make('CartPole-v0')
 
-    for _ in range(1000):
-        for training_step in range(100):
-            obs = env.reset()
-            for i in count():
-                #  env.render()
-                pos, vel, angle, angle_vel = obs
-                #  step = 1 if angle_vel >= 0 else 0
-                step = random.randrange(0, 2)
-                obs, reward, done, info = env.step(step)
-                print(sess.run(neuro, feed_dict={input_: obs[None, :]}))
-                if done:
-                    if i > 100:
-                        print(i)
-                    break
+    dp0dW = optimizer.compute_gradients(neuro[0, 0])
+    dp1dW = optimizer.compute_gradients(neuro[0, 1])
+    training_op = TrainingOp(optimizer, dp0dW)
+
+    sess = tf.InteractiveSession()
+    init.run()
+    for training_episode in range(10000):
+        obs = env.reset()
+        states, actions = [obs], []
+
+        for training_step in range(1000):
+            env.render()
+            pos, vel, angle, angle_vel = obs
+            #  step = 1 if angle_vel >= 0 else 0
+            step = random.randrange(0, 2)
+            actions.append(step)
+            obs, reward, done, info = env.step(step)
+            states.append(obs)
+            if done:
+                import ipdb; ipdb.set_trace()
+                grads_per_state = [
+                                   [g.eval(feed_dict={input_: state[None, :]}) for g, v in dp0dW]
+                                   for state in states
+                                  ]
+                calculate_mean_gradient(states, actions, input_, grads_per_state)
+                break
 
 
 if __name__ == "__main__":
