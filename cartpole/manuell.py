e166cbf4a0cd60150aae25d0563da805425113bb
Gabriel Kabbe <gabriel.kabbe@mail.de>
2017-11-05 00:34:02 +0100

    Update main

--- cartpole/manuell.py
+++ cartpole/manuell.py
@@ -61,29 +61,26 @@ def discounted_reward(immediate_rewards, discount_factor=0.9):
 
 
 def main():
+    no_of_training_episodes = 10_000
+    no_of_training_steps = 1000
     reward_gameover = -10
     reward_stillplaying = 1
-    input_, neuro = Neuro()
+    neuro = Neuro()
     init = tf.global_variables_initializer()    
 
     env = gym.make('CartPole-v0')
 
-    dp0dW = tf.gradients(neuro[0, 0], tf.trainable_variables())
-    #  dp1dW = optimizer.compute_gradients(neuro[0, 1])
-    updated_grads = [tf.placeholder(tf.float32, shape=weight.get_shape()) 
-                     for weight in tf.trainable_variables()]
-    training_op = [tf.assign(weight, update) 
-                   for weight, update in zip(tf.trainable_variables(), updated_grads)]
-
     sess = tf.InteractiveSession()
     init.run()
     evaluations = []
-    for training_episode in range(10000):
+    for training_episode in range(1, no_of_training_episodes + 1):
+        logger.info("Training episode %i", training_episode)
         obs = env.reset()
         states, actions, immediate_rewards = [obs], [], []
 
-        for training_step in range(1000):
-            env.render()
+        for training_step in range(no_of_training_steps):
+            if RENDER:
+                env.render()
             pos, vel, angle, angle_vel = obs
             #  step = 1 if angle_vel >= 0 else 0
             step = random.randrange(0, 2)
@@ -92,10 +89,13 @@ def main():
             states.append(obs)
             if done:
                 immediate_rewards.append(reward_gameover)
-                evalu = evaluate_game(sess, states, actions, immediate_rewards)
+                evalu = evaluate_game(states, actions, immediate_rewards)
                 evaluations += evalu
                 break
             immediate_rewards.append(reward_stillplaying)
+
+        if training_episode % 100 == 0:
+            neuro.update_gradients(*zip(*evaluations))
 
 
 if __name__ == "__main__":
