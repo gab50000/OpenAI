c842631a88a72256349916548153c8b388584f17
Gabriel Kabbe <gabriel.kabbe@mail.de>
2017-11-08 11:43:29 +0100

    More updates

--- cartpole/manuell.py
+++ cartpole/manuell.py
@@ -1,7 +1,8 @@
-from itertools import count
+from collections import deque
 import random
 
 import gym
+
 import numpy as np
 import tensorflow as tf
 import daiquiri
@@ -18,12 +19,12 @@ class Neuro:
     def __init__(self):
 
         n_in = 4
-        n_hidden = 3
+        n_hidden = 4
         n_out = 2
 
         initializer = tf.contrib.layers.variance_scaling_initializer()
         self.input_ = tf.placeholder(dtype=tf.float32, shape=[None, n_in])
-        hidden = tf.layers.dense(self.input_, n_hidden, activation=tf.nn.sigmoid)
+        hidden = tf.layers.dense(self.input_, n_hidden, activation=tf.nn.tanh)
         self.probs = tf.nn.softmax(tf.layers.dense(hidden, 2))
         self.gradients = tf.gradients(self.probs[0, 0], tf.trainable_variables())
 
@@ -39,7 +40,7 @@ class Neuro:
     def update_gradients(self, states, actions, scores):
         scores = np.array(scores)
         scores = (scores - scores.mean()) / scores.std()
-        training_step = 0.01
+        training_step = 0.005
         sess = self.sess
         logger.debug("Calculating gradients")
         grads = [sess.run(self.gradients, feed_dict={self.input_: state[None, :]})
@@ -47,9 +48,10 @@ class Neuro:
         
         weights = tf.trainable_variables()
         logger.debug("Calculating weight updates")
+        score_signs = np.sign(scores)
         for grad, action, score in zip(grads, actions, scores):
             sign = 1 - (2 * action)
-            weights = [w + sign * np.sign(score) * training_step * g 
+            weights = [w + score * training_step * g 
                        for w, g in zip(weights, grad)]
         logger.debug("Apply weight updates")
         sess.run([tf.assign(w, update) for w, update in zip(tf.trainable_variables(), weights)])
@@ -62,7 +64,6 @@ def calculate_mean_gradient(states, actions, input_, dp0dW):
 
 
 def evaluate_game(states, actions, immediate_rewards):
-    evaluation = []
     dr = discounted_reward(immediate_rewards)
     return [(state, action, r) for state, action, r in zip(states, actions, dr)]
 
@@ -76,10 +77,10 @@ def discounted_reward(immediate_rewards, discount_factor=0.9):
 
 
 def main():
-    no_of_training_episodes = 10_000
+    no_of_training_episodes = 1_000_000
     no_of_training_steps = 1000
-    update_frequency = 100
-    reward_gameover = -10
+    update_frequency = 10
+    reward_gameover = -5
     reward_stillplaying = 1
     neuro = Neuro()
     init = tf.global_variables_initializer()    
@@ -88,13 +89,15 @@ def main():
 
     sess = tf.InteractiveSession()
     init.run()
-    evaluations = []
+    evaluations = deque(maxlen=1000)
+    step_counter = 0
     for training_episode in range(1, no_of_training_episodes + 1):
         logger.info("Training episode %i", training_episode)
         obs = env.reset()
         states, actions, immediate_rewards = [obs], [], []
 
         for training_step in range(no_of_training_steps):
+            step_counter += 1
             if RENDER:
                 env.render()
             #  pos, vel, angle, angle_vel = obs
@@ -104,17 +107,17 @@ def main():
             obs, reward, done, info = env.step(step)
             states.append(obs)
             if done:
-                immediate_rewards.append(reward_gameover)
+                immediate_rewards.append(reward)
                 evalu = evaluate_game(states, actions, immediate_rewards)
                 evaluations += evalu
                 break
-            immediate_rewards.append(reward_stillplaying)
+            immediate_rewards.append(reward)
 
         if training_episode % update_frequency == 0:
-            logger.info("Average number of training steps: %f", len(evaluations) / update_frequency)
+            # logger.info("Average number of training steps: %f", len(evaluations) / training_episode)
+            logger.info("Steps in this episode: %f", step_counter / update_frequency)
             neuro.update_gradients(*zip(*evaluations))
-            evaluations = []
-
+            step_counter = 0
 
 if __name__ == "__main__":
     main()
